5331187f52e5aba44b31a673808af57cf882c7db
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2024-12-21 21:50:25 -0400

    feat: Added LoadDurationVariable and LoadSecondsVariable functions

--- env/errors.go
+++ env/errors.go
@@ -5,4 +5,5 @@ import "errors"
 var (
 	EnvironmentVariableNotFoundError      = "environment variable not found: %v"
 	FailedToLoadEnvironmentVariablesError = errors.New("failed to load environment variables")
+	InvalidDurationError                  = "invalid key '%v' duration value: %v"
 )
--- env/loader.go
+++ env/loader.go
@@ -3,6 +3,7 @@ package env
 import (
 	"fmt"
 	"os"
+	"time"
 )
 
 // LoadVariable load variable from environment variables
@@ -13,4 +14,30 @@ func LoadVariable(key string) (uri string, err error) {
 		return "", fmt.Errorf(EnvironmentVariableNotFoundError, key)
 	}
 	return variable, nil
+}
+
+// LoadDurationVariable load duration variable from environment variables
+func LoadDurationVariable(key string) (duration time.Duration, err error) {
+	// Get environment variable
+	variable, err := LoadVariable(key)
+	if err != nil {
+		return 0, err
+	}
+
+	// Parse the duration
+	duration, err = time.ParseDuration(variable)
+	if err != nil {
+		return 0, fmt.Errorf(InvalidDurationError, key, variable)
+	}
+	return duration, nil
+}
+
+// LoadSecondsVariable load duration variable in seconds from environment variables
+func LoadSecondsVariable(key string) (seconds float64, err error) {
+	// Get the duration
+	duration, err := LoadDurationVariable(key)
+	if err != nil {
+		return 0, err
+	}
+	return duration.Seconds(), nil
 }
