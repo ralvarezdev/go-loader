d47e9f19d14ce8cdf01927aac5ad44df3d97e8ea
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-01-03 20:54:13 -0400

    feat: added Loader interface and default implementation
    * Added Loader interface and default implementation
    * Added Loader's LoadIntVariable function

--- env/errors.go
+++ env/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrEnvironmentVariableNotFound      = "environment variable not found: %v"
 	ErrFailedToLoadEnvironmentVariables = errors.New("failed to load environment variables")
 	ErrInvalidDuration                  = "invalid key '%v' duration value: %v"
+	ErrInvalidInteger                   = "invalid key '%v' integer value: %v"
 )
--- env/loader.go
+++ env/loader.go
@@ -3,11 +3,36 @@ package env
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
+type (
+	// Loader interface for loading environment variables
+	Loader interface {
+		LoadVariable(key string) (uri string, err error)
+		LoadDurationVariable(key string) (duration time.Duration, err error)
+		LoadSecondsVariable(key string) (seconds float64, err error)
+		LoadIntVariable(key string) (value int, err error)
+	}
+
+	// DefaultLoader struct
+	DefaultLoader struct{}
+)
+
+// NewDefaultLoader creates a new default environment variable loader
+func NewDefaultLoader(loadFn func() error) (*DefaultLoader, error) {
+	// Execute the load function
+	if loadFn != nil {
+		if err := loadFn(); err != nil {
+			return nil, ErrFailedToLoadEnvironmentVariables
+		}
+	}
+	return &DefaultLoader{}, nil
+}
+
 // LoadVariable load variable from environment variables
-func LoadVariable(key string) (uri string, err error) {
+func (d *DefaultLoader) LoadVariable(key string) (uri string, err error) {
 	// Get environment variable
 	variable, exists := os.LookupEnv(key)
 	if !exists {
@@ -17,9 +42,12 @@ func LoadVariable(key string) (uri string, err error) {
 }
 
 // LoadDurationVariable load duration variable from environment variables
-func LoadDurationVariable(key string) (duration time.Duration, err error) {
+func (d *DefaultLoader) LoadDurationVariable(key string) (
+	duration time.Duration,
+	err error,
+) {
 	// Get environment variable
-	variable, err := LoadVariable(key)
+	variable, err := d.LoadVariable(key)
 	if err != nil {
 		return 0, err
 	}
@@ -33,11 +61,30 @@ func LoadDurationVariable(key string) (duration time.Duration, err error) {
 }
 
 // LoadSecondsVariable load duration variable in seconds from environment variables
-func LoadSecondsVariable(key string) (seconds float64, err error) {
+func (d *DefaultLoader) LoadSecondsVariable(key string) (
+	seconds float64,
+	err error,
+) {
 	// Get the duration
-	duration, err := LoadDurationVariable(key)
+	duration, err := d.LoadDurationVariable(key)
 	if err != nil {
 		return 0, err
 	}
 	return duration.Seconds(), nil
+}
+
+// LoadIntVariable load integer variable from environment variables
+func (d *DefaultLoader) LoadIntVariable(key string) (value int, err error) {
+	// Get environment variable
+	variable, err := d.LoadVariable(key)
+	if err != nil {
+		return 0, err
+	}
+
+	// Parse the integer
+	value, err = strconv.Atoi(variable)
+	if err != nil {
+		return 0, fmt.Errorf(ErrInvalidInteger, key, variable)
+	}
+	return value, nil
 }
