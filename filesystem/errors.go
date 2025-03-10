7569a1a96dc411a01420d19b0297eb8059fefa52
Ramón Álvarez <86166683+ralvarezdev@users.noreply.github.com>
2025-03-10 12:07:42 -0400

    feat: added read csv file function

--- filesystem/errors.go
+++ filesystem/errors.go
@@ -1,5 +1,10 @@
 package filesystem
 
+import (
+	"errors"
+)
+
 var (
 	ErrUnableToReadFile = "unable to read file: %v"
+	ErrNilFile          = errors.New("file cannot be nil")
 )
--- filesystem/read_files.go
+++ filesystem/read_files.go
@@ -1,6 +1,7 @@
 package filesystem
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
 )
@@ -12,4 +13,68 @@ func ReadFile(path string) ([]byte, error) {
 		return nil, fmt.Errorf(ErrUnableToReadFile, err)
 	}
 	return file, nil
+}
+
+// OpenFile opens the file from the given path
+func OpenFile(path string) (*os.File, error) {
+	// Open the CSV file
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open file: %w", err)
+	}
+	return file, nil
+}
+
+// CloseFile closes the file
+func CloseFile(file *os.File) error {
+	if file != nil {
+		err := file.Close()
+		if err != nil {
+			return fmt.Errorf("failed to close file: %w", err)
+		}
+	}
+	return nil
+}
+
+// ReadCSVFile reads a CSV file and returns a slice of string slices
+func ReadCSVFile(file *os.File, readHeaders bool) (
+	*[][]string,
+	*[]string,
+	error,
+) {
+	// Check if the file is nil
+	if file == nil {
+		return nil, nil, ErrNilFile
+	}
+
+	// Defer closing the file
+	defer func(file *os.File) {
+		if err := CloseFile(file); err != nil {
+			fmt.Println(err)
+		}
+	}(file)
+
+	// Create a new CSV reader
+	reader := csv.NewReader(file)
+
+	// Read the header line
+	var headers []string
+	var err error
+	if readHeaders {
+		headers, err = reader.Read()
+		if err != nil {
+			return nil, nil, fmt.Errorf("failed to read header line: %w", err)
+		}
+	}
+
+	// Read all records from the CSV file
+	records, err := reader.ReadAll()
+	if err != nil {
+		return nil, nil, fmt.Errorf("failed to read CSV file: %w", err)
+	}
+
+	if readHeaders {
+		return &records, &headers, nil
+	}
+	return &records, nil, nil
 }
